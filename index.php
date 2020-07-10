8177407c98b84207fb13be25171658c715c98322
Shishupal <shishupal614@gmail.com>
2020-07-10 10:34:21 +0530

    Second

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 <body bgcolor="#1E90FF">
-<h1 align="center"><font color="#E6E6FA">Task Completed</h1>
+<h1 align="center"><font color="#E6E6FA">Task Completed!!</h1>
 <?php
 echo "Thank Vimal Sir for such tasks";
 echo "Congratulation!!! your php code test successfull"
