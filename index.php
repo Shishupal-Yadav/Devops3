38193ced5c750468ef32a6103f59fc3baa2231b9
Shishupal <shishupal614@gmail.com>
2020-07-16 10:44:17 +0530

    Done

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
 <body bgcolor="#1E90FF">
 <h1 align="center"><font color="#E6E6FA">Task Completed :-)</h1>
 <?php
-echo "Thank Vimal Sir for such tasks";
-echo "Congratulation!!! your php code test successfull"
+echo nl2br("Thanks Vimal Sir for such tasks.\nCongratulation!!! Your php code test successfull.")
 ?>
 </body>
 </html>
